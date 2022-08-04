54295ae1a2e84166ae8286a2548e4ae6adf920fa
Hiran Raj <hirankvlr@gmail.com>
2022-08-05 00:48:13 +0530

    Flight ticket booking section started and still working on it

--- src/Components/FlightTicket/FlightTicketBooking.js
+++ src/Components/FlightTicket/FlightTicketBooking.js
@@ -1,21 +1,72 @@
 import React from "react";
 import "./FlightTicket-Booking.css";
+import { BsArrowLeftRight } from "react-icons/bs";
+import { FiArrowRight } from "react-icons/fi";
 function FlightTicketBooking() {
   return (
     <div className="flight-ticket-div">
       <div className="flight-ticket-container">
-        <h5 className="flight-container-title">book Flight ticket</h5>
-        <div className="trip-selection">
-          <h6>trip</h6>
+        {/* title */}
+
+        <h5 className="flight-container-title">Book Flight ticket</h5>
+
+        {/* trip selection area */}
+
+        <div className="flight-trip-selection">
+          <button className="one-way">
+            <h6>One way</h6>
+          </button>
+        
+            <button className="round-way">
+              <h6>Round Trip</h6>
+            </button>
+        
         </div>
-        <div className="place-selection">
-          <h6>place</h6>
+
+        {/* place selection area */}
+
+        <div className="flight-place-selection">
+          <div className="depart-from-div">
+            <h6 className="depart from">depart From</h6>
+            <h5 className="depart-from-place">Ernakulam Jn</h5>
+          </div>
+          <div className="place-shift-area">
+            <div className="shift-arrow">
+              <BsArrowLeftRight />
+            </div>
+          </div>
+          <div className="going-to-div">
+            <h6 className="going-top">Going To</h6>
+            <h5 className="going-to-place">Chengannur</h5>
+          </div>
         </div>
-        <div className="date-selection">
-          <h6>date</h6>
+
+        {/* date selection area */}
+
+        <div className="flight-date-selection">
+          <div className="depature-div">
+            <h6 className="depature-date-div">depature date</h6>
+            <h5 className="depature-date">5 Aug' 22</h5>
+            <h6 className="depature-day">Friday</h6>
+          </div>
+
+          <div className="return-div">
+            <h6 className="return-date-div">depature date</h6>
+            <h5 className="return-date">5 Aug' 22</h5>
+            <h6 className="return-day">Friday</h6>
+          </div>
         </div>
-        <div className="travellers-economy">
+
+        {/* economy selection area */}
+
+        <div className="flight-travellers-economy">
           <h6>traveller-economy</h6>
+        </div>
+
+        <div className="search-flight-div">
+          <button id="search-btn" type="button" class="btn btn-danger">
+            Search Flights <FiArrowRight />{" "}
+          </button>
         </div>
       </div>
     </div>
