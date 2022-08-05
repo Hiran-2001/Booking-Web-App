0909f1f29c585663a303151fcfaa7be1ef866408
Hiran Raj <hirankvlr@gmail.com>
2022-08-05 20:11:03 +0530

    Flight ticket section modified

--- src/Components/FlightTicket/FlightTicketBooking.js
+++ src/Components/FlightTicket/FlightTicketBooking.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FlightTicket-Booking.css";
 import { BsArrowLeftRight } from "react-icons/bs";
 import { FiArrowRight } from "react-icons/fi";
 function FlightTicketBooking() {
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowNav(!showNav);
+  };
   return (
     <div className="flight-ticket-div">
       <div className="flight-ticket-container">
@@ -16,11 +21,10 @@ function FlightTicketBooking() {
           <button className="one-way">
             <h6>One way</h6>
           </button>
-        
-            <button className="round-way">
-              <h6>Round Trip</h6>
-            </button>
-        
+
+          <button className="round-way">
+            <h6>Round Trip</h6>
+          </button>
         </div>
 
         {/* place selection area */}
@@ -28,11 +32,13 @@ function FlightTicketBooking() {
         <div className="flight-place-selection">
           <div className="depart-from-div">
             <h6 className="depart from">depart From</h6>
-            <h5 className="depart-from-place">Ernakulam Jn</h5>
+            <h5 id="" className="depart-from-place">
+              Ernakulam Jn
+            </h5>
           </div>
           <div className="place-shift-area">
             <div className="shift-arrow">
-              <BsArrowLeftRight />
+              <BsArrowLeftRight onClick={toggleSidebar} />
             </div>
           </div>
           <div className="going-to-div">
@@ -61,6 +67,25 @@ function FlightTicketBooking() {
 
         <div className="flight-travellers-economy">
           <h6>traveller-economy</h6>
+          <div className="traveller-selection-div">
+            <div className="adult-selection">
+              <h6>adult</h6>
+              <div className="plus-minus-section">
+                <button type="button" class="btn btn-outline-dark">
+                  -
+                </button>
+                <div className="adult travellers">
+
+                <h6>1</h6>
+                </div>
+                <button type="button" class="btn btn-outline-dark">
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="child-selection"></div>
+            <div className="infant-selection"></div>
+          </div>
         </div>
 
         <div className="search-flight-div">
