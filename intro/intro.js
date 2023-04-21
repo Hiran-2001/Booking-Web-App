8b2b120e5702a7e4c1f26e61c2cfb8208df777fc
Hiran Raj <hirankvlr@gmail.com>
2023-04-21 21:12:41 +0530

    working on typescript intro

---
+++ intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Hiran", age: 21 };
+console.log("Hiran");
+console.log(user.email);
---
+++ intro/intro.ts
@@ -0,0 +1,5 @@
+let user = {name :"Hiran" , age : 21}
+
+console.log("Hiran");
+console.log(user.email);
+
